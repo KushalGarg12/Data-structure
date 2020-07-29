071be252b01ec90b4f1c1168b1c985569d5c2b30
KushalGarg12 <66581889+KushalGarg12@users.noreply.github.com>
2020-07-29 22:46:43 +0530

    Add files via upload

---
+++ Linked list/Creating a linked list/main.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct node
+{
+    int num;
+    struct node*link;
+}*m ;
+void createnode(int n)
+{
+    struct node *temp,*r;
+    int num,i;
+    m=(struct node*)malloc(sizeof(struct node));
+    if(m==NULL)
+    {
+
+        printf("No memory allocation");
+
+    }
+    else
+    {
+        printf("Enter the data for node 1=");
+        scanf("%d",&num);
+        m->num=num;
+        m->link=NULL;
+        temp=m;
+
+        for(i=2;i<=n;i++)
+        {
+             r=(struct node*)malloc(sizeof(struct node));
+             if(r==NULL)
+             {
+
+                printf("No memory allocation");
+                break;
+             }
+             else
+             {
+                 printf("Enter the data for node %d=",i);
+                 scanf("%d",&num);
+                 r->num=num;
+                 r->link=NULL;
+                 temp->link=r;
+                 temp=temp->link;
+             }
+        }
+    }
+}
+void displaynode()
+{
+    struct node* tmp;
+    if(m==NULL)
+    {
+        printf("No memory allocation");
+    }
+    else
+    {
+        tmp=m;
+        while(tmp!=NULL)
+        {
+            printf("\n%d\n",tmp->num);
+            tmp=tmp->link;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of nodes=");
+    scanf("%d",&n);
+    createnode(n);
+    printf("Data entered=");
+    displaynode();
+    printf("\n");
+    return 0;
+}
